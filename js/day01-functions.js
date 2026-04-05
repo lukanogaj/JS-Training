e4d3a514bf7c239a3228d2c8136aeda0f756308e
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-04-05 13:50:44 +0100

    functions-return

--- js/day01-functions.js
+++ js/day01-functions.js
@@ -6,6 +6,88 @@ const todos = [
 ];
 
 /// 05.04.2026 Sunday
+const deleteTodo = (todos, id) => {
+	return todos.filter((todo) => todo.id !== id);
+};
+
+///
+const getCompletedTodos = (todos) => {
+	return todos.filter((todo) => todo.completed);
+};
+
+//
+const groupTodos = (todos) => {
+	return todos.reduce(
+		(acc, todo) => {
+			if (todo.completed) {
+				acc.completed.push(todo);
+			} else {
+				acc.active.push(todo);
+			}
+			return acc;
+		},
+		{ completed: [], active: [] },
+	);
+};
+//
+const getCompletedTitles = (todos) => {
+	return todos.filter((todo) => todo.completed).map((todo) => todo.title);
+};
+
+//
+const countActiveTodos = (todos) => {
+	return todos.reduce((count, todo) => {
+		if (!todo.completed) {
+			count++;
+		}
+		return count;
+	}, 0);
+};
+
+//
+const getTodosStats = (todos) => {
+	return todos.reduce(
+		(acc, todo) => {
+			acc.total++;
+
+			if (todo.completed) {
+				acc.completed++;
+			} else {
+				acc.active++;
+			}
+
+			return acc;
+		},
+		{ total: 0, completed: 0, active: 0 },
+	);
+};
+//
+const getActiveTitles = (todos) => {
+	return todos.reduce((acc, todo) => {
+		if (!todo.completed) {
+			acc.push(todo.title);
+		}
+		return acc;
+	}, []);
+};
+
+//
+const getOverdueTitles = (todos) => {
+	const today = new Date();
+	today.setHours(0, 0, 0, 0);
+
+	return todos.reduce((acc, todo) => {
+		const todoDate = new Date(todo.dueDate);
+		todoDate.setHours(0, 0, 0, 0);
+
+		if (!todo.completed && todoDate < today) {
+			acc.push(todo.title);
+		}
+		return acc;
+	}, []);
+};
+
+//
 
 const groupTodosByStatus = (todos) => {
 	const today = new Date();
